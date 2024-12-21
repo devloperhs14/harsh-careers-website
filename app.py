f0eb44858c8110f202e16245fd41a0b93a7f9000
Harsh Shukla <devloper.hs2015@gmail.com>
2024-12-21 13:44:28 +0000

    Automatic commit before Assistant edits

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 # import libs
-from flask import Flask
+from flask import Flask, render_template
 
-## create a app class from Flask
+# create a app class from Flask
+
 # name variable refers to how a praticular script is called/invoked - changes depending on where its invoked
 app = Flask(__name__)
 
 # add a route - url/route
 @app.route('/') # decorator
 def hello_world():
-  return 'Hello World!'
+  return render_template('home.html')
 
 # auto run
 if __name__ == '__main__':
